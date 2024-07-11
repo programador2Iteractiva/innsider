532c92042a92bdf565a514276b206c771397ec81
Edwin González <programador1@interactiva.loc>
2024-07-11 18:00:10 -0500

    update changes for this files         new file:   wp-content/themes/novoinnsider/assets/images/Icono-innsider-white.png        new file:   wp-content/themes/novoinnsider/assets/scss/pages/login/login.scss        modified:   wp-content/themes/novoinnsider/assets/scss/pages/pages.scss        modified:   wp-content/themes/novoinnsider/header.php        new file:   wp-content/themes/novoinnsider/pages/page-login.php        modified:   wp-content/themes/novoinnsider/style.css        new file:   wp-content/themes/novoinnsider/template-parts/template-pages/template-page-login.php        new file:   wp-content/uploads/2024/07/Banner-login-150x150.jpg        new file:   wp-content/uploads/2024/07/Banner-login-150x150.png        new file:   wp-content/uploads/2024/07/Banner-login-300x108.jpg        new file:   wp-content/uploads/2024/07/Banner-login-300x108.png        new file:   wp-content/uploads/2024/07/Banner-login-768x276.jpg        new file:   wp-content/uploads/2024/07/Banner-login-768x276.png        new file:   wp-content/uploads/2024/07/Banner-login.jpg        new file:   wp-content/uploads/2024/07/Banner-login.

--- wp-content/themes/novoinnsider/header.php
+++ wp-content/themes/novoinnsider/header.php
@@ -10,71 +10,72 @@
 
 <body>
 
-    <?php if (!is_page('registro') && !is_page('login')) : ?>
-        <header>
-            <div class="container">
-                <nav class="navbar navbar-expand-lg py-4">
-                    <div class="container-fluid">
-                        <div class="d-flex justify-content-center align-items-center">
-                            <div class="d-block">
-                                <?php if (has_custom_logo()) : ?>
-                                    <?php the_custom_logo(); ?>
-                                <?php endif; ?>
-                            </div>
+    <header>
+        <div class="container">
+            <nav class="navbar navbar-expand-lg py-4">
+                <div class="container-fluid">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <div class="d-block">
+                            <?php if (has_custom_logo()) : ?>
+                                <?php the_custom_logo(); ?>
+                            <?php endif; ?>
                         </div>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#main-menu" aria-controls="main-menu">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div class="offcanvas offcanvas-end" tabindex="-1" id="main-menu" aria-labelledby="main-menu-label">
-                            <div class="offcanvas-header">
-                                <h5 class="offcanvas-title" id="main-menu-label">Menu</h5>
-                                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                            </div>
-                            <div class="offcanvas-body d-lg-flex d-block justify-content-lg-center align-items-lg-center">
-                                <?php
-                                wp_nav_menu(array(
-                                    'theme_location' => 'primary',
-                                    'container' => false,
-                                    'menu_class' => 'navbar-nav',
-                                    'fallback_cb' => '__return_false',
-                                    'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                                    'depth' => 3,
-                                    'walker' => new bootstrap_5_wp_nav_menu_walker()
-                                ));
-                                ?>
-                                <ul class="d-block d-lg-none navbar-nav justify-content-end flex-grow-1 pe-3">
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" id="userNameResponsive" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"></a>
-                                        <ul class="dropdown-menu logout">
-                                            <li><i class="fa-solid fa-power-off mx-3"></i> Cerrar Sesion</li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="d-none d-lg-block row m-0 p-0">
-                            <div class="class-btns-register d-flex justify-content-center">
-                                <div class="d-flex justify-content-center align-items-center flex-row container-btn-login">
-                                    <a class="btn-login mx-3" id="btn-login">Inicio</a>
-                                    <a class="btn-register mx-1" id="btn-register">Registro</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="d-block d-lg-none col-12">
-                            <div class="col-12 mt-4 d-flex justify-content-center align-items-center">
-                                <div class="d-flex justify-content-center align-items-center flex-row container-btn-login">
-                                    <a class="btn-login mx-1" id="btn-login">Inicio</a>
-                                    <a class="btn-register mx-1" id="btn-register">Registro</a>
-                                </div>
-                            </div>
-                        </div>
-
                     </div>
-                </nav>
-            </div>
-        </header>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#main-menu" aria-controls="main-menu">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="offcanvas offcanvas-end" tabindex="-1" id="main-menu" aria-labelledby="main-menu-label">
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="main-menu-label">Menu</h5>
+                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body d-lg-flex d-block justify-content-lg-center align-items-lg-center">
+                            <?php
+                            wp_nav_menu(array(
+                                'theme_location' => 'primary',
+                                'container' => false,
+                                'menu_class' => 'navbar-nav',
+                                'fallback_cb' => '__return_false',
+                                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                                'depth' => 3,
+                                'walker' => new bootstrap_5_wp_nav_menu_walker()
+                            ));
+                            ?>
+                            <ul class="d-block d-lg-none navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" id="userNameResponsive" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"></a>
+                                    <ul class="dropdown-menu logout">
+                                        <li><i class="fa-solid fa-power-off mx-3"></i> Cerrar Sesion</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="d-none d-lg-block row m-0 p-0">
+                        <div class="class-btns-register d-flex justify-content-center">
+                            <div class="d-flex justify-content-center align-items-center flex-row container-btn-login">
+                                <a class="btn-login mx-3" id="btn-login">Inicio</a>
+                                <a class="btn-register mx-1" id="btn-register">Registro</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="d-block d-lg-none col-12">
+                        <div class="col-12 mt-4 d-flex justify-content-center align-items-center">
+                            <div class="d-flex justify-content-center align-items-center flex-row container-btn-login">
+                                <a class="btn-login mx-1" id="btn-login">Inicio</a>
+                                <a class="btn-register mx-1" id="btn-register">Registro</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <?php if (!is_page('registro') && !is_page('login')) : ?>
 
     <?php endif; ?>

fdb1bca59032ded6c6f93f0140d6385dde22a0ca
EdwinG <programador1@interactiva.net.co>
2024-07-29 13:10:32 -0500

    include new code for wp-content/themes/novoinnsider/template-parts/template-pages/template-page-academy.php

--- wp-content/themes/novoinnsider/template-parts/template-pages/template-page-academy.php
+++ wp-content/themes/novoinnsider/template-parts/template-pages/template-page-academy.php
@@ -3,13 +3,14 @@
 /**
  * Template for page Academy
  */
+$category = get_queried_object();
 ?>
 
 <div>
     <div class="container my-5 mb-0">
         <div class="row d-flex justify-content-center align-align-items-center mb-4">
             <div class="col-12 d-flex flex-lg-row">
-                <h2 class="title">ACADEMIA</h2>
+                <h1 class="NotoSans-Bold">ACADEMIA</h1>
                 <div class="col-11 mx-1" id="linea">
                     <hr class="mx-5 px-4">
                 </div>
@@ -48,17 +49,68 @@
                         </div>
 
                     </div>
-                </div>
+            </div>
             </a>
-            <!-- <div class="col-lg-6">
-                <img src="<?= get_template_directory_uri() . '/assets/images/NOTICIA-01.png'; ?>" alt="Podcast">
-                <div class="card-info mt-3">
-                    <div class="w-75 p-2 mb-2" style="border-radius: 0.5rem; background: #001965; color: white;"><i class="fa-solid fa-newspaper mx-2"></i></i>Infografias</div>
-                    <h2 class="font-weight-bold">Lorem Ipsum 1</h2>
-                    <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-                    <p class="time">Video | 5 min</p>
-                </div>
-            </div> -->
+            <?php
+            /* Consulta en base de datos que valida si la categoria cuenta con status activo, es decir si el campo True/False tiene
+                check mostrara el conteido, de lo contrario lo ocultara  */
+
+                global $wpdb;
+
+                $tableTermmeta = $wpdb->prefix . 'termmeta';
+
+                $metaKey = 'Status_Categories';
+                $metaValue = '1';
+
+                $allData = $wpdb->prepare("SELECT *  FROM {$tableTermmeta} WHERE `meta_key` = '{$metaKey}' AND `meta_VALUE` = '{$metaValue}'");
+
+                $allCategoriesWithStatusActive = $wpdb->get_results($wpdb->prepare($allData));
+
+            ?>
+
+            <?php if(isset($allCategoriesWithStatusActive) && !empty($allCategoriesWithStatusActive)) : ?> 
+
+                <?php foreach($allCategoriesWithStatusActive as $CategoriesWithStatusActive) : ?>
+
+                    <?php $idCategoriesWithStatusActive = $CategoriesWithStatusActive->term_id; ?>
+
+                    <?php $listCategoriesAcademy = get_terms(
+                        array(
+                            'taxonomy' => 'academia',
+                            'hide_empty' => true,
+                            'order' => 'DESC'
+                            )
+                        ) 
+                    ?>
+
+                    <?php if(isset($listCategoriesAcademy) && !empty($listCategoriesAcademy)) : ?>
+                        <?php foreach($listCategoriesAcademy as $listCategoryAcademy) : ?>
+                            <?php if($listCategoryAcademy->term_id == $idCategoriesWithStatusActive) : ?>
+                                <div class="col-lg-6 mb-4">
+                                    <a href="<?= get_term_link($listCategoryAcademy->term_id)  ?>" style="text-decoration: none;">
+                                        <?php $imageCategoryAcademy = get_field('Category_Image', $listCategoryAcademy); ?>
+                                        <img src="<?php echo wp_get_attachment_image_url($imageCategoryAcademy, 'full', '',['style' => 'object-fit: cover;']); ?>" alt="Podcast">
+                                        <div class="card-info mt-3">
+                                            <div class="d-flex align-items-start flex-lg-row flex-column">
+                                                <div class="col-4">
+                                                    <h2 class="font-weight-bold"><?= $listCategoryAcademy->name; ?></h2>
+                                                    <p><?= $listCategoryAcademy->description; ?></p>
+                                                </div>
+                                                <div class="col-4">
+                                                    <div class="w-75 p-1 mb-2" style="border-radius: 2.5rem; background: #001965; color: white; text-align: center;">Ver más</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            <?php endif; ?>
+                        <?php endforeach ?>
+                    <?php endif ?>
+
+                <?php endforeach; ?>
+
+            <?php endif ?> 
+
         </div>
     </div>
 </div>

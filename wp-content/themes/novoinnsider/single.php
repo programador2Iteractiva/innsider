cce07856281775e76229dd84451c4c6f9672e33f
Edwin González <programador1@interactiva.loc>
2024-07-30 15:28:31 -0500

    include new code for file single.php relation to template events national

--- wp-content/themes/novoinnsider/single.php
+++ wp-content/themes/novoinnsider/single.php
@@ -5,43 +5,165 @@
  */
 
 get_header();
+
+
+$taxId = isset($_GET['tax']) ? intval($_GET['tax']) : 0;
+$moduleId = isset($_GET['module_id']) ? intval($_GET['module_id']) : 0;
+$contentId = isset($_GET['content_id']) ? intval($_GET['content_id']) : 0;
+
 ?>
 
-<div class="container mx-auto px-0">
-    <div class="container mt-4 mx-0 px-0 pb-4">
+<div class="container mx-5 mx-lg-auto px-0">
+    <div class="mt-4 mx-0 px-0 pb-4">
         <?php custom_breadcrumbs(); ?>
     </div>
 </div>
 
-<?php if (have_posts()) : ?>
-    <?php while (have_posts()) : ?>
-        <?php the_post(); ?>
-        <?php $category = get_the_category(); ?>
+<div class="container mx-auto px-0">
+    <div class="mt-4 mx-lg-0 mx-2 px-0 pb-4">
+        <div class="row m-0 p-0"></div>
 
-        <div class="container-single-post" style="background: whitesmoke">
+            <?php
+                $listPostAcademy = new WP_Query
+                (
+                    [
+                        'post__in' => [$moduleId],
+                        'tax_query' => array(
+                            array(
+                                'taxonomy' => 'academia',
+                                'field' => 'id',
+                                'terms' => $taxId,
+                            )
+                        ),
+                        'orderby' => 'post_date',
+                        'order' => 'ASC',
+                        'posts_per_page' => -1,
+                        'post_status' => 'publish'
+                    ]
+                );
+            ?>
+                    
+            <?php if(isset($listPostAcademy) && !empty($listPostAcademy)) : ?> 
+                <?php if($listPostAcademy->have_posts()) : ?>
 
-            <?php if ($category[0]->term_id == 10): ?>
-                <div class="container-single-image">
-                    <?= the_post_thumbnail('', ['class' => 'single-img']); ?>
-                </div>
-            <?php else: ?>
+                    <?php while($listPostAcademy->have_posts()) : $listPostAcademy->the_post() ?>
 
-                <a href="#" onclick="saveLogsClick('<?php esc_html(the_title())?>', '<?= $category[0]->name?>'); openVideo('<?= get_field('video_post') ?>', event);">
-                    <div class="container-single-image">
-                        <?php the_post_thumbnail('', ['class' => 'single-img']); ?>
-                    </div>
-                </a>
+                        <?php $SubtitleModule = get_field('Subtitle_Module'); ?>
+                        <?php $postActivityId = get_the_ID(); ?>
 
-            <?php endif; ?>
-            <div>
-                <h2 class="single-title"><?= the_title(); ?></h2>
-            </div>
-            <div class="container-single-text">
-                <?= the_content(); ?>
-            </div>
-        </div>
+                        <?php $listContentModules = get_field('list_of_content_module'); ?>
 
-    <?php endwhile; ?>
-<?php endif; ?>
+                        <?php if(isset($listContentModules) && !empty($listContentModules)) : ?>
+
+                            <?php $counter = 0; ?>
+                            <?php $counter = 0; ?>
+                            <?php $specificModule = null; ?>
+                            <?php $otherModules = []; ?>
+
+                            <?php foreach ($listContentModules as $index => $listContentModule) : ?>
+                                <?php if ($index == $contentId) : ?>
+                                    <?php $specificModule = $listContentModule; ?>
+                                <?php else : ?>
+                                    <?php $otherModules[$index] = $listContentModule; ?>
+                                <?php endif ?>
+                            <?php endforeach; ?>
+
+                            <?php if ($specificModule) : ?>
+                                <!-- Mostrar el contenido del módulo específico -->
+                                <?php $imageModuleAcademy = $specificModule['Img_Video_Mod']; ?>
+                                <?php $titleModuleAcademy = $specificModule['Title_Video_Mod']; ?>
+                                <?php $speakerModuleAcademy = $specificModule['Name_Speaker_Mod']; ?>
+                                <?php $descriptionModuleAcademy = $specificModule['Description_Module']; ?>
+                                <?php $urlModuleAcademy = $specificModule['URL_Video_Module']; ?>
+                                <?php $bannerContentModule = $specificModule['Banner_Content_Module']; ?>
+                                <?php $titleVideoContentMod = $specificModule['Title_Video_Content_Mod']; ?>
+                                <?php $DescriptionContentModule = $specificModule['Description_Content_Module']; ?>
+                                <?php $thumbnailUrl = obtenerMiniaturaVimeo($urlModuleAcademy);  ?>
+
+                                <?php if (isset($titleModuleAcademy) && !empty($titleModuleAcademy)) : ?>
+                                    <h3 class="NotoSans-Bold title-color"><?= esc_html($titleModuleAcademy); ?></h3>
+                                <?php endif; ?>
+
+                                <?php if (isset($descriptionModuleAcademy) && !empty($descriptionModuleAcademy)) : ?>
+                                    <p class="NotoSans-Regular description-color"><?= esc_html($descriptionModuleAcademy); ?></p>
+                                <?php endif; ?> 
+
+                                <div class="container background-taxonomy mt-lg-5 mt-4 px-5">
+                                    <div class="container banner-single">
+                                        <?php if (isset($thumbnailUrl) && !empty($thumbnailUrl)) : ?>  
+                                            <img src="<?= esc_url($thumbnailUrl); ?>" alt="Herramientas" class="bg-single">  
+                                        <?php elseif (isset($bannerContentModule) && !empty($bannerContentModule)) : ?>
+                                            <img src="<?= esc_url(wp_get_attachment_url($bannerContentModule)); ?>" alt="Herramientas" class="bg-single"> 
+                                        <?php endif; ?>   
+
+                                        <div class="wrapper-single"></div>
+                                    </div>
+                                    <div class="container mt-4">
+                                        <div class="row m-0 p-0">
+                                            <?php if (isset($titleVideoContentMod) && !empty($titleVideoContentMod)) : ?>
+                                                <h1 class="NotoSans-Bold title-color mb-3 text-uppercase"><?= esc_html($titleVideoContentMod); ?></h1>
+                                            <?php endif; ?>
+                                            <?php if (isset($DescriptionContentModule) && !empty($DescriptionContentModule)) : ?>
+                                                <h5 class="NotoSans-SemiBold description-color line-height-2 text-align-justify mb-lg-5 mb-2"><?= esc_html($DescriptionContentModule); ?></h5>
+                                            <?php endif; ?>
+                                        </div>
+                                    </div>
+                                </div> 
+
+                            <?php endif; ?>
+
+                            <div class="col-12 mx-1" id="linea">
+                                <hr>
+                            </div>
+
+                            <div class="col-12 col-md-4 col-lg-3 col-xl-3 col-xxl-3 col-xxxl-3 d-flex flex-lg-row flex-column justify-content-start align-items-start container-card-category m-0 p-0 mt-3 mb-3">
+
+                                <?php foreach ($otherModules as $index => $listContentModule) : ?>
+                                    
+                                    <?php $imageModuleAcademy = $listContentModule['Img_Video_Mod']; ?>
+                                    <?php $titleModuleAcademy = $listContentModule['Title_Video_Mod']; ?>
+                                    <?php $speakerModuleAcademy = $listContentModule['Name_Speaker_Mod']; ?>
+                                    <?php $descriptionModuleAcademy = $listContentModule['Description_Module']; ?>
+                                    <?php $urlModuleAcademy = $listContentModule['URL_Video_Module']; ?>
+                                    <?php $bannerContentModule = $listContentModule['Banner_Content_Module']; ?>
+                                    <?php $titleVideoContentMod = $listContentModule['Title_Video_Content_Mod']; ?>
+                                    <?php $DescriptionContentModule = $listContentModule['Description_Content_Module']; ?>
+                                    <?php $thumbnailUrl = obtenerMiniaturaVimeo($urlModuleAcademy);  ?>
+
+                                    <div class="col-11 d-flex justify-content-center align-items-center">
+                                        <a href="<?= esc_url(get_permalink($postActivityId) . '?module_id=' . $postActivityId . '&content_id=' . $index . '&tax=' . $taxId); ?>" style="text-decoration: none;">
+                                            <div class="card bg-transparent" style="border: none !important">
+                                                <?php if ($imageModuleAcademy) : ?>
+                                                    <img class="img-card-event" src="<?= esc_url(wp_get_attachment_url($imageModuleAcademy)); ?>" alt="Podcast">
+                                                <?php endif; ?>
+                                                <div class="card-info mt-lg-4 mt-3 p-0">
+                                                    <div class="w-75 p-2 mb-2" style="border-radius: 0.5rem; background: #001965; color: white;">
+                                                        <i class="fa-regular fa-circle-play mx-2"></i>Ver ahora
+                                                    </div>
+                                                    <?php if ($titleModuleAcademy) : ?>
+                                                        <h5 class="NotoSans-Bold title-color"><?= esc_html($titleModuleAcademy); ?></h5>
+                                                    <?php endif; ?>
+                                                    <?php if ($speakerModuleAcademy) : ?>
+                                                        <p class="NotoSans-Regular description-color"><?= esc_html($speakerModuleAcademy); ?></p>
+                                                    <?php endif; ?>
+                                                </div>
+                                            </div>
+                                        </a>
+                                    </div>
+
+                                    <?php $counter++; ?>
+                                <?php endforeach; ?>
+
+                            </div>
+
+                        <?php endif; ?>  
+
+                    <?php endwhile; ?>
+                <?php endif; ?>
+            <?php endif; ?> 
+
+        </div>  
+    </div>
+</div>    
 
 <?php get_footer(); ?>

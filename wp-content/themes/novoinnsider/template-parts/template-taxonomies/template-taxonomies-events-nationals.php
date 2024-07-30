2a75588b8cb7d58b5820832a256c78adca33f51d
Edwin González <programador1@interactiva.loc>
2024-07-30 15:27:50 -0500

    include new code for this file wp-content/themes/novoinnsider/template-parts/template-taxonomies/template-taxonomies-events-nationals.php

--- wp-content/themes/novoinnsider/template-parts/template-taxonomies/template-taxonomies-events-nationals.php
+++ wp-content/themes/novoinnsider/template-parts/template-taxonomies/template-taxonomies-events-nationals.php
@@ -8,7 +8,7 @@
 
 $taxonomy = get_queried_object();
 ?>
-<div class="container mx-auto px-0">
+<div class="container mx-5 mx-lg-auto px-0">
     <div class="container mt-4 mx-0 px-0 pb-4">
         <?php custom_breadcrumbs(); ?>
     </div>
@@ -28,7 +28,7 @@ $taxonomy = get_queried_object();
 </div>
 
 <div class="container mx-auto px-0">
-    <div class="container mt-4 mx-0 px-0 pb-4">
+    <div class="container mt-4 mx-lg-0 mx-2 px-0 pb-4">
         <div class="row m-0 p-0">
 
             <?php
@@ -57,9 +57,18 @@ $taxonomy = get_queried_object();
                         <?php $SubtitleModule = get_field('Subtitle_Module'); ?>
                         <?php $postActivityId = get_the_ID(); ?>
 
+                        <h3 class="NotoSans-Bold title-color"><?= the_title() ?></h3>
+                        <h2 class="NotoSans-Bold m-0 text-uppercase text-linear-gradient"><?= $SubtitleModule; ?></h2>
+
+                        <div class="col-12 mx-1" id="linea">
+                            <hr>
+                        </div>
+
                         <?php $listContentModules = get_field('list_of_content_module'); ?>
 
                         <?php if(isset($listContentModules) && !empty($listContentModules)) : ?>
+
+                            <?php $counter = 0; ?>
 
                             <?php foreach($listContentModules as $listContentModule) : ?>
 
@@ -69,33 +78,28 @@ $taxonomy = get_queried_object();
                                 <?php $descriptionModuleAcademy = $listContentModule['Description_Module']; ?>
                                 <?php $urlModuleAcademy = $listContentModule['URL_Video_Module']; ?>
 
-                                <h3 class="NotoSans-Bold title-color"><?= the_title() ?></h3>
-                                <h2 class="NotoSans-Bold m-0 text-uppercase text-linear-gradient"><?= $SubtitleModule; ?></h2>
-
-                                <div class="col-12 mx-1" id="linea">
-                                    <hr>
-                                </div>
-
-                                <div class="col-12 col-md-4 col-lg-3 col-xl-3 col-xxl-3 col-xxxl-3 d-flex flex-column justify-content-center align-items-center container-card-category m-0 p-0 mt-3 mb-3">
+                                <div class="col-12 col-md-4 col-lg-3 col-xl-3 col-xxl-3 col-xxxl-3 d-flex flex-column justify-content-start align-items-center container-card-category m-0 p-0 mt-3 mb-3">
                                     <div class="col-11 d-flex justify-content-center align-items-center">
-                                        <a href="<?php echo get_permalink($postActivityId); ?>" style="text-decoration: none;">
+                                        <a href="<?php echo get_permalink($postActivityId) . '?module_id=' . $postActivityId . '&content_id=' . $counter . '&tax=' . $taxonomy->term_id; ?>" style="text-decoration: none;">
                                             <div class="card" style="border: none !important">
                                                 <?php if($imageModuleAcademy) : ?>
-                                                    <img src="<?php echo wp_get_attachment_url($imageModuleAcademy); ?>" alt="Podcast">
+                                                    <img class="img-card-event" src="<?php echo wp_get_attachment_url($imageModuleAcademy); ?>" alt="Podcast">
                                                 <?php endif; ?>
-                                                <div class="card-info mt-1">
+                                                <div class="card-info mt-lg-4 mt-3 p-0">
                                                     <div class="w-75 p-2 mb-2" style="border-radius: 0.5rem; background: #001965; color: white;"><i class="fa-regular fa-circle-play mx-2"></i>Ver ahora</div>
                                                     <?php if($titleModuleAcademy) : ?>
-                                                        <h2 class="NotoSans-Bold title-color"><?= $titleModuleAcademy; ?></h2>
+                                                        <h5 class="NotoSans-Bold title-color"><?= $titleModuleAcademy; ?></h5>
                                                     <?php endif; ?>
                                                     <?php if($speakerModuleAcademy) : ?>
-                                                        <p class="NotoSans-SemiBold description-color"><?= $speakerModuleAcademy; ?></p>
+                                                        <p class="NotoSans-Regular description-color"><?= $speakerModuleAcademy; ?></p>
                                                     <?php endif; ?>     
                                                 </div>
                                             </div>
                                         </a>
                                     </div>
                                 </div>
+
+                                <?php $counter++; ?>
 
                             <?php endforeach; ?>
 

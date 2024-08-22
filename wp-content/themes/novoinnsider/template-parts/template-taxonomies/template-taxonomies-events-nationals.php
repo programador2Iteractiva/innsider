1c1b60e94f4da9ed090ab5292877535c5b612fc9
Edwin González <programador1@interactiva.loc>
2024-08-22 16:17:27 -0500

    include new changes for this file wp-content/themes/novoinnsider/template-parts/template-taxonomies/template-taxonomies-events-nationals.php  to view subcategories to this content

--- wp-content/themes/novoinnsider/template-parts/template-taxonomies/template-taxonomies-events-nationals.php
+++ wp-content/themes/novoinnsider/template-parts/template-taxonomies/template-taxonomies-events-nationals.php
@@ -8,119 +8,112 @@
 
 $taxonomy = get_queried_object();
 ?>
-<div class="container mx-5 mx-lg-auto px-0">
-    <div class="container mt-4 mx-0 px-0 pb-4">
-        <?php custom_breadcrumbs(); ?>
-    </div>
-</div>
 
 
-<?php $descriptioonCategory = $taxonomy->description; ?>
-<?php $subtitleCategory = get_field('title_for_description_complementary', $taxonomy); ?>
-<?php $bannerCategory = get_field('Category_Image_Banner', $taxonomy); ?>
+<?php if (isset($taxonomy->term_id) && !empty($taxonomy->term_id)) : ?>
 
+    <?php $currentTermId = $taxonomy->term_id; ?>
 
-<div class="container background-taxonomy mt-lg-3 mt-3 px-5">
-    <div class="container banner-taxonomy-academy">
-        <?php if(isset($bannerCategory) && !empty($bannerCategory)) : ?>
-            <img src="<?= esc_url(wp_get_attachment_url($bannerCategory)); ?>" alt="Herramientas" class="bg-taxonomy-academy"> 
-        <?php endif; ?>    
-        <div class="wrapper-taxonomy-academy"></div>
-    </div>
-    <div class="container mt-4">
-        <div class="row m-0 p-0">
-            <?php if(isset($descriptioonCategory) && !empty($descriptioonCategory)) : ?>
-                <h1 class="NotoSans-Bold title-color mb-3 text-uppercase"><?= $descriptioonCategory; ?></h1>
-            <?php endif ?>
-            <?php if(isset($subtitleCategory) && !empty($subtitleCategory)) : ?>
-                <h5 class="NotoSans-SemiBold description-color line-height-2 text-align-justify mb-lg-5 mb-2"><?= $subtitleCategory; ?></h5>
-            <?php endif ?>
+    <?php
+
+    $subcategories = get_terms(
+        array(
+            'taxonomy' => $taxonomy->taxonomy,
+            'hide_empty' => false,
+            'parent' => $currentTermId,
+            'order' => 'ASC',
+        )
+    )
+
+    ?>
+
+    <?php if (!empty($subcategories) && !is_wp_error($subcategories)) : ?>
+
+        <div class="container mx-5 mx-lg-auto px-0">
+            <div class="container mt-4 mx-0 px-0 pb-4">
+                <?php custom_breadcrumbs(); ?>
+            </div>
         </div>
-    </div>
-</div>
 
-<div class="container mx-auto px-0">
-    <div class="container mt-4 mx-lg-0 mx-2 px-0 pb-4">
-        <div class="row m-0 p-0">
 
-            <?php
-                $listPostAcademy = new WP_Query
-                (
-                    [
-                        'tax_query' => array(
-                            array(
-                                'taxonomy' => 'academia',
-                                'field' => 'id',
-                                'terms' => $taxonomy->term_id,
-                            )
-                        ),
-                        'orderby' => 'post_date',
-                        'order' => 'ASC',
-                        'posts_per_page' => -1,
-                        'post_status' => 'publish'
-                    ]
-                );
-            ?>
-        
-            <?php if(isset($listPostAcademy) && !empty($listPostAcademy)) : ?> 
-                <?php if($listPostAcademy->have_posts()) : ?>
-                    <?php while($listPostAcademy->have_posts()) : $listPostAcademy->the_post() ?>
+        <?php $descriptioonCategory = $taxonomy->description; ?>
+        <?php $subtitleCategory = get_field('title_for_description_complementary', $taxonomy); ?>
+        <?php $bannerCategory = get_field('Category_Image_Banner', $taxonomy); ?>
 
-                        <?php $SubtitleModule = get_field('Subtitle_Module'); ?>
-                        <?php $postActivityId = get_the_ID(); ?>
 
-                        <h3 class="NotoSans-Bold title-color"><?= the_title() ?></h3>
-                        <h2 class="NotoSans-Bold m-0 text-uppercase text-linear-gradient"><?= $SubtitleModule; ?></h2>
+        <div class="container background-taxonomy mt-lg-3 mt-3 px-5">
+            <div class="container banner-taxonomy-academy">
+                <?php if (isset($bannerCategory) && !empty($bannerCategory)) : ?>
+                    <img src="<?= esc_url(wp_get_attachment_url($bannerCategory)); ?>" alt="Herramientas" class="bg-taxonomy-academy">
+                <?php endif; ?>
+                <div class="wrapper-taxonomy-academy"></div>
+            </div>
+            <div class="container mt-4">
+                <div class="row m-0 p-0">
+                    <?php if (isset($descriptioonCategory) && !empty($descriptioonCategory)) : ?>
+                        <h1 class="NotoSans-Bold title-color mb-3 text-uppercase"><?= $descriptioonCategory; ?></h1>
+                    <?php endif ?>
+                    <?php if (isset($subtitleCategory) && !empty($subtitleCategory)) : ?>
+                        <h5 class="NotoSans-SemiBold description-color line-height-2 text-align-justify mb-lg-5 mb-2"><?= $subtitleCategory; ?></h5>
+                    <?php endif ?>
+                </div>
+            </div>
+        </div>
 
-                        <div class="col-12 mx-1" id="linea">
-                            <hr>
-                        </div>
+        <div class="container">                        
+            <div class="row p-3 m-3 d-flex justify-content-center align-items-start">
 
-                        <?php $listContentModules = get_field('list_of_content_module'); ?>
+                <?php foreach ($subcategories as $subcategory) :  ?>
 
-                        <?php if(isset($listContentModules) && !empty($listContentModules)) : ?>
+                    <?php $listIdWithNationalEvents = $CategoriesWithNationalEvents->term_id; ?>
 
-                            <?php $counter = 0; ?>
+                    <div class="col-12 col-md-4 col-lg-3 d-flex flex-column justify-content-center align-items-center container-card-category-impact m-0 p-0 mt-3 mb-5 pb-4 mx-4">
 
-                            <?php foreach($listContentModules as $listContentModule) : ?>
+                        <a href="<?php echo get_term_link($subcategory->term_id) ?>">
+                            <div class="mb-4 figure">
+                                <?php $imageSubcategoryAcademy = get_field('Category_Image', $subcategory); ?>
 
-                                <?php $imageModuleAcademy = $listContentModule['Img_Video_Mod']; ?>
-                                <?php $titleModuleAcademy = $listContentModule['Title_Video_Mod']; ?>
-                                <?php $speakerModuleAcademy = $listContentModule['Name_Speaker_Mod']; ?>
-                                <?php $descriptionModuleAcademy = $listContentModule['Description_Module']; ?>
-                                <?php $urlModuleAcademy = $listContentModule['URL_Video_Module']; ?>
-
-                                <div class="col-12 col-md-4 col-lg-3 col-xl-3 col-xxl-3 col-xxxl-3 d-flex flex-column justify-content-start align-items-center container-card-category m-0 p-0 mt-3 mb-3">
-                                    <div class="col-11 d-flex justify-content-center align-items-center">
-                                        <a href="<?php echo get_permalink($postActivityId) . '?module_id=' . $postActivityId . '&content_id=' . $counter . '&tax=' . $taxonomy->term_id; ?>" style="text-decoration: none;">
-                                            <div class="card" style="border: none !important">
-                                                <?php if($imageModuleAcademy) : ?>
-                                                    <img class="img-card-event" src="<?php echo wp_get_attachment_url($imageModuleAcademy); ?>" alt="Podcast">
-                                                <?php endif; ?>
-                                                <div class="card-info mt-lg-4 mt-3 p-0">
-                                                    <div class="w-75 p-2 mb-2" style="border-radius: 0.5rem; background: #001965; color: white;"><i class="fa-regular fa-circle-play mx-2"></i>Ver ahora</div>
-                                                    <?php if($titleModuleAcademy) : ?>
-                                                        <h5 class="NotoSans-Bold title-color"><?= $titleModuleAcademy; ?></h5>
-                                                    <?php endif; ?>
-                                                    <?php if($speakerModuleAcademy) : ?>
-                                                        <p class="NotoSans-Regular description-color"><?= $speakerModuleAcademy; ?></p>
-                                                    <?php endif; ?>     
-                                                </div>
-                                            </div>
-                                        </a>
+                                <?php if ($imageSubcategoryAcademy) :  ?>
+                                    <?php echo wp_get_attachment_image($imageSubcategoryAcademy, 'full', '', ['style' => 'object-fit: cover']); ?>
+                                <?php endif ?>
+                            </div>
+                            <div class="info_description col-12">
+                                <div class="col-12 h-100">
+                                    <div class="col-12 h-100">
+                                        <div class="d-flex justify-content-start align-items-start flex-column">
+                                            <h3 class="NotoSans-Bold title-color"><?= $subcategory->name; ?></h3>
+                                            <p class="description-color NotoSans-Regular"><?= $subcategory->description; ?></p>
+                                        </div>
+                                    </div>
+                                    <div class="col-12 d-flex justify-content-center align-items-center">
+                                        <div class="w-75">
+                                            <div class="w-100 p-2 mb-2 btn-view-more">Ver más</div>
+                                        </div>
                                     </div>
                                 </div>
+                            </div>
+                        </a>
+                    </div>
 
-                                <?php $counter++; ?>
 
-                            <?php endforeach; ?>
+                <?php endforeach ?>
 
-                        <?php endif; ?>  
-
-                    <?php endwhile; ?>
-                <?php endif; ?>
-            <?php endif; ?> 
-
+            </div>
         </div>
-    </div>
-</div>
+
+
+        <div class="container mx-5 mx-lg-auto px-0">
+            <h5 class="NotoSans-Bold title-color">CO24UMA00061</h5>
+        </div>
+
+        <div class="col-12 border p-5 background-section-logo-innsider">
+            <div class="col-12 d-flex justify-content-center align-items-center p-4">
+                <?php if (has_custom_logo()) : ?>
+                    <?php the_custom_logo(); ?>
+                <?php endif; ?>
+            </div>
+        </div>
+
+    <?php endif ?>
+
+<?php endif; ?>

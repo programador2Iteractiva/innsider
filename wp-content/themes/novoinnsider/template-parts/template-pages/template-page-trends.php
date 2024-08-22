3b0c417e893f1edb43c15d184f54abdc196b995b
Edwin González <programador1@interactiva.loc>
2024-08-22 08:32:03 -0500

    include new process to ths view

--- wp-content/themes/novoinnsider/template-parts/template-pages/template-page-trends.php
+++ wp-content/themes/novoinnsider/template-parts/template-pages/template-page-trends.php
@@ -21,15 +21,15 @@ $content = get_the_content();
     <div class="container banner-academy">
         <?php the_post_thumbnail('', ['class' => 'bg-banner-academy']) ?>
         <div class="wrapper-banner-academy">
-            <div class="container-text-banner-academy">
+            <!-- <div class="container-text-banner-academy">
                 <p class="text-transform-uppercase">
                     <?php the_title(); ?>
                 </p>
-            </div>
+            </div> -->
             <h4 class="text-white mt-3"><?php the_content(); ?></h4>
-            <div class="container-text-banner-academy w-100 h-100 m-auto d-flex justify-content-lg-start align-items-center">
+            <!-- <div class="container-text-banner-academy w-100 h-100 m-auto d-flex justify-content-lg-start align-items-center">
                 <img src="<?= get_template_directory_uri() . '/assets/images/Icono-innsider-white.png'; ?>" alt="Herramientas" class="bg-banner-single-category">
-            </div>
+            </div> -->
         </div>
     </div>
 

36cb749fb96519911482874ac843b233226e8227
Edwin González <programador1@interactiva.loc>
2024-08-26 17:29:30 -0500

    include new process to conditional content relational taxonmies subcategories in this file wp-content/themes/novoinnsider/template-parts/template-taxonomies/template-subcategories-taxonomies/template-taxonomies-subcategories-events.php

--- wp-content/themes/novoinnsider/template-parts/template-taxonomies/template-subcategories-taxonomies/template-taxonomies-subcategories-events.php
+++ wp-content/themes/novoinnsider/template-parts/template-taxonomies/template-subcategories-taxonomies/template-taxonomies-subcategories-events.php
@@ -7,120 +7,148 @@
  */
 
 $taxonomy = get_queried_object();
+$cuttentTaxonomyId = $taxonomy->term_taxonomy_id;
+$cuttentTaxonomyParentId = $taxonomy->parent;
+
 ?>
-<div class="container mx-5 mx-lg-auto px-0">
+<div class="container mx-2 mx-lg-auto px-0">
     <div class="container mt-4 mx-0 px-0 pb-4">
         <?php custom_breadcrumbs(); ?>
     </div>
 </div>
 
-
 <?php $descriptioonCategory = $taxonomy->description; ?>
 <?php $subtitleCategory = get_field('title_for_description_complementary', $taxonomy); ?>
 <?php $bannerCategory = get_field('Category_Image_Banner', $taxonomy); ?>
 
-
-<div class="container background-taxonomy mt-lg-3 mt-3 px-5">
-    <div class="container banner-taxonomy-academy">
-        <?php if(isset($bannerCategory) && !empty($bannerCategory)) : ?>
-            <img src="<?= esc_url(wp_get_attachment_url($bannerCategory)); ?>" alt="Herramientas" class="bg-taxonomy-academy"> 
-        <?php endif; ?>    
-        <div class="wrapper-taxonomy-academy"></div>
-    </div>
-    <div class="container mt-4">
-        <div class="row m-0 p-0">
-            <?php if(isset($descriptioonCategory) && !empty($descriptioonCategory)) : ?>
-                <h1 class="NotoSans-Bold title-color mb-3 text-uppercase"><?= $descriptioonCategory; ?></h1>
-            <?php endif ?>
-            <?php if(isset($subtitleCategory) && !empty($subtitleCategory)) : ?>
-                <h5 class="NotoSans-SemiBold description-color line-height-2 text-align-justify mb-lg-5 mb-2"><?= $subtitleCategory; ?></h5>
-            <?php endif ?>
+<?php /* template con el contenido de las subcategoria de la categoria princiapl "Eventos Nacionales" term_id 6 */  ?>
+<?php if(!empty($cuttentTaxonomyParentId) && $cuttentTaxonomyParentId == 6) : ?>
+    <div class="container background-taxonomy mt-lg-3 mt-3 px-5">
+        <div class="container banner-taxonomy-academy">
+            <?php if(isset($bannerCategory) && !empty($bannerCategory)) : ?>
+                <img src="<?= esc_url(wp_get_attachment_url($bannerCategory)); ?>" alt="Herramientas" class="bg-taxonomy-academy"> 
+            <?php endif; ?>    
+            <div class="wrapper-taxonomy-academy"></div>
+        </div>
+        <div class="container mt-4">
+            <div class="row m-0 p-0">
+                <?php if(isset($descriptioonCategory) && !empty($descriptioonCategory)) : ?>
+                    <h1 class="NotoSans-Bold title-color mb-3 text-uppercase"><?= $descriptioonCategory; ?></h1>
+                <?php endif ?>
+                <?php if(isset($subtitleCategory) && !empty($subtitleCategory)) : ?>
+                    <h5 class="NotoSans-SemiBold description-color line-height-2 text-align-justify mb-lg-5 mb-2"><?= $subtitleCategory; ?></h5>
+                <?php endif ?>
+            </div>
         </div>
     </div>
-</div>
 
-<div class="container mx-auto px-0">
-    <div class="container mt-4 mx-lg-0 mx-2 px-0 pb-4">
-        <div class="row m-0 p-0">
+    <div class="container mx-auto px-0">
+        <div class="container mt-4 mx-lg-0 mx-2 px-0 pb-4">
+            <div class="row m-0 p-0">
 
-            <?php
-                $listPostAcademy = new WP_Query
-                (
-                    [
-                        'tax_query' => array(
-                            array(
-                                'taxonomy' => 'academia',
-                                'field' => 'id',
-                                'terms' => $taxonomy->term_id,
-                            )
-                        ),
-                        'orderby' => 'post_date',
-                        'order' => 'ASC',
-                        'posts_per_page' => -1,
-                        'post_status' => 'publish'
-                    ]
-                );
-            ?>
-        
-            <?php if(isset($listPostAcademy) && !empty($listPostAcademy)) : ?> 
-                <?php if($listPostAcademy->have_posts()) : ?>
-                    <?php while($listPostAcademy->have_posts()) : $listPostAcademy->the_post() ?>
+                <?php
+                    $listPostAcademy = new WP_Query
+                    (
+                        [
+                            'tax_query' => array(
+                                array(
+                                    'taxonomy' => 'academia',
+                                    'field' => 'id',
+                                    'terms' => $taxonomy->term_id,
+                                )
+                            ),
+                            'orderby' => 'post_date',
+                            'order' => 'ASC',
+                            'posts_per_page' => -1,
+                            'post_status' => 'publish'
+                        ]
+                    );
+                ?>
+            
+                <?php if(isset($listPostAcademy) && !empty($listPostAcademy)) : ?> 
+                    <?php if($listPostAcademy->have_posts()) : ?>
+                        <?php while($listPostAcademy->have_posts()) : $listPostAcademy->the_post() ?>
 
-                        <?php $SubtitleModule = get_field('Subtitle_Module'); ?>
-                        <?php $postActivityId = get_the_ID(); ?>
+                            <?php $SubtitleModule = get_field('Subtitle_Module'); ?>
+                            <?php $postActivityId = get_the_ID(); ?>
 
-                        <h3 class="NotoSans-Bold title-color"><?= the_title() ?></h3>
-                        <h2 class="NotoSans-Bold m-0 text-uppercase text-linear-gradient"><?= $SubtitleModule; ?></h2>
+                            <h3 class="NotoSans-Bold title-color"><?= the_title() ?></h3>
+                            <h2 class="NotoSans-Bold m-0 text-uppercase text-linear-gradient"><?= $SubtitleModule; ?></h2>
 
-                        <div class="col-12 mx-1" id="linea">
-                            <hr>
-                        </div>
+                            <div class="col-12 mx-1" id="linea">
+                                <hr>
+                            </div>
 
-                        <?php $listContentModules = get_field('list_of_content_module'); ?>
+                            <?php $listContentModules = get_field('list_of_content_module'); ?>
 
-                        <?php if(isset($listContentModules) && !empty($listContentModules)) : ?>
+                            <?php if(isset($listContentModules) && !empty($listContentModules)) : ?>
 
-                            <?php $counter = 0; ?>
+                                <?php $counter = 0; ?>
 
-                            <?php foreach($listContentModules as $listContentModule) : ?>
+                                <?php foreach($listContentModules as $listContentModule) : ?>
 
-                                <?php $imageModuleAcademy = $listContentModule['Img_Video_Mod']; ?>
-                                <?php $titleModuleAcademy = $listContentModule['Title_Video_Mod']; ?>
-                                <?php $speakerModuleAcademy = $listContentModule['Name_Speaker_Mod']; ?>
-                                <?php $descriptionModuleAcademy = $listContentModule['Description_Module']; ?>
-                                <?php $urlModuleAcademy = $listContentModule['URL_Video_Module']; ?>
+                                    <?php $imageModuleAcademy = $listContentModule['Img_Video_Mod']; ?>
+                                    <?php $titleModuleAcademy = $listContentModule['Title_Video_Mod']; ?>
+                                    <?php $speakerModuleAcademy = $listContentModule['Name_Speaker_Mod']; ?>
+                                    <?php $descriptionModuleAcademy = $listContentModule['Description_Module']; ?>
+                                    <?php $urlModuleAcademy = $listContentModule['URL_Video_Module']; ?>
 
-                                <div class="col-12 col-md-4 col-lg-3 col-xl-3 col-xxl-3 col-xxxl-3 d-flex flex-column justify-content-start align-items-center container-card-category m-0 p-0 mt-3 mb-3">
-                                    <div class="col-11 d-flex justify-content-center align-items-center">
-                                        <a href="<?php echo get_permalink($postActivityId) . '?module_id=' . $postActivityId . '&content_id=' . $counter . '&tax=' . $taxonomy->term_id; ?>" style="text-decoration: none;">
-                                            <div class="card" style="border: none !important">
-                                                <?php if($imageModuleAcademy) : ?>
-                                                    <img class="img-card-event" src="<?php echo wp_get_attachment_url($imageModuleAcademy); ?>" alt="Podcast">
-                                                <?php endif; ?>
-                                                <div class="card-info mt-lg-4 mt-3 p-0">
-                                                    <div class="w-75 p-2 mb-2" style="border-radius: 0.5rem; background: #001965; color: white;"><i class="fa-regular fa-circle-play mx-2"></i>Ver ahora</div>
-                                                    <?php if($titleModuleAcademy) : ?>
-                                                        <h5 class="NotoSans-Bold title-color"><?= $titleModuleAcademy; ?></h5>
+                                    <div class="col-12 col-md-4 col-lg-3 col-xl-3 col-xxl-3 col-xxxl-3 d-flex flex-column justify-content-start align-items-center container-card-category m-0 p-0 mt-3 mb-3">
+                                        <div class="col-11 d-flex justify-content-center align-items-center">
+                                            <a href="<?php echo get_permalink($postActivityId) . '?module_id=' . $postActivityId . '&content_id=' . $counter . '&tax=' . $taxonomy->term_id; ?>" style="text-decoration: none;">
+                                                <div class="card" style="border: none !important">
+                                                    <?php if($imageModuleAcademy) : ?>
+                                                        <img class="img-card-event" src="<?php echo wp_get_attachment_url($imageModuleAcademy); ?>" alt="Podcast">
                                                     <?php endif; ?>
-                                                    <?php if($speakerModuleAcademy) : ?>
-                                                        <p class="NotoSans-Regular description-color"><?= $speakerModuleAcademy; ?></p>
-                                                    <?php endif; ?>     
+                                                    <div class="card-info mt-lg-4 mt-3 p-0">
+                                                        <div class="w-75 p-2 mb-2" style="border-radius: 0.5rem; background: #001965; color: white;"><i class="fa-regular fa-circle-play mx-2"></i>Ver ahora</div>
+                                                        <?php if($titleModuleAcademy) : ?>
+                                                            <h5 class="NotoSans-Bold title-color"><?= $titleModuleAcademy; ?></h5>
+                                                        <?php endif; ?>
+                                                        <?php if($speakerModuleAcademy) : ?>
+                                                            <p class="NotoSans-Regular description-color"><?= $speakerModuleAcademy; ?></p>
+                                                        <?php endif; ?>     
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </a>
+                                            </a>
+                                        </div>
                                     </div>
-                                </div>
 
-                                <?php $counter++; ?>
+                                    <?php $counter++; ?>
 
-                            <?php endforeach; ?>
+                                <?php endforeach; ?>
 
-                        <?php endif; ?>  
+                            <?php endif; ?>  
 
-                    <?php endwhile; ?>
-                <?php endif; ?>
-            <?php endif; ?> 
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+                <?php endif; ?> 
 
+            </div>
         </div>
     </div>
-</div>
+<?php /* template con el contenido de las subcategoria de la categoria princiapl "Cursos de formación" term_id 5 */  ?>
+<?php elseif(!empty($cuttentTaxonomyParentId) && $cuttentTaxonomyParentId == 5) : ?>
+    <div class="container">
+        <div class="row m-0 pt-5 pb-5 p-0 w-100 h-auto row-section">
+            <div class="col-12 d-flex justify-content-center align-items-center container-toolbox">
+                <main class="main white">
+                    <div class="container">
+                        <article class="conpes">
+                            <div class="row justify-content-evenly justify-content-md-center">
+                                <div class="container-not-found text-center p-5">
+                                    <i class="fas fa-exclamation-circle text-white mt-5"></i>
+                                    <h2 class="text-not-found text-white mb-5">Aún no hay contenido disponible para este curso.</h2>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </main>
+            </div>
+        
+        </div>
+    </div>
+<?php endif; ?>
+
+
+

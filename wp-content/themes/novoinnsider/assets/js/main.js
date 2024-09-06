d3c746e370661bc7bb56433b93857d55fdf779aa
edwingonzalezm <programador1@interactiva.net.co>
2024-09-06 14:46:30 -0500

    include one import to this file wp-content/themes/novoinnsider/assets/js/main.js  to js boostrap

--- wp-content/themes/novoinnsider/assets/js/main.js
+++ wp-content/themes/novoinnsider/assets/js/main.js
@@ -3,6 +3,7 @@ import Swal from "sweetalert2";
 import Swiper from 'swiper/bundle';
 import 'bootstrap/dist/js/bootstrap.min';
 import '@fortawesome/fontawesome-free/js/all';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 window.Swal = Swal;

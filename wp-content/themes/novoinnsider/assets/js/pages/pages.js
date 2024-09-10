938d98e8f469434eed3c9c3b4577ad8598b79756
edwingonzalezm <programador1@interactiva.net.co>
2024-09-10 17:57:39 -0500

    include new code for this file wp-content/themes/novoinnsider/assets/js/pages/pages.js

--- wp-content/themes/novoinnsider/assets/js/pages/pages.js
+++ wp-content/themes/novoinnsider/assets/js/pages/pages.js
@@ -2,6 +2,7 @@
 
 import './home/home'
 import {saveClickImput, saveClickCities} from './register/register'
+import './help-support/help-support'
 
 window.saveClickImput = saveClickImput;
 window.saveClickCities = saveClickCities;

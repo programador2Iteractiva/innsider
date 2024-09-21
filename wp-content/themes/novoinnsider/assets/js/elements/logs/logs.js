7ff2910dcd9364c0b0008d0557273bf91a17be8c
edwingonzalezm <programador1@interactiva.net.co>
2024-09-21 17:42:56 -0500

    include new code in this new file for a new process logs in js wp-content/themes/novoinnsider/assets/js/elements/logs/logs.js

---
+++ wp-content/themes/novoinnsider/assets/js/elements/logs/logs.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+
+export function saveLogsClick(actionurl){
+    // $.post({
+    //     url: ajax_object.ajax_url,
+    //     method: 'POST',
+    //     data: {
+    //         action: 'save_logs_click',
+    //         nonce: ajax_object.ajax_nonce,
+    //         actionurl: actionurl,
+    //     },
+    //     success: function () {
+    //         console.log('Save');
+    //     },
+    //     error: function () {
+    //         console.log('Error');
+    //     }
+    // })
+
+    alert('hola');
+}

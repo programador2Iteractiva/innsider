f19bc1e53691acafe4f938e1452c422a35298392
programador2Iteractiva <programador2@interactiva.net.co>
2025-10-16 16:11:51 -0500

    Delete wp-content/themes/novoinnsider/assets directory

--- wp-content/themes/novoinnsider/assets/js/elements/elements.js
+++
@@ -1,8 +0,0 @@
-import {playVideo, redirectVideo} from './player/player'
-import {saveLogsClick} from  './logs/logs'
-import './colors/colors'
-
-
-window.playVideo = playVideo;
-window.saveLogsClick = saveLogsClick
-window.redirectVideo = redirectVideo;
